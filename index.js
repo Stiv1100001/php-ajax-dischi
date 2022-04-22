2a8269583808cbf8b446fb74180401376beec5bb
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-04-22 14:28:33 +0200

    Bonus

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const app = new Vue({
   el: "#app",
   data: () => ({
     dischi: [],
+    genre: "",
   }),
+  methods: {
+    search() {
+      axios.get("/php-ajax-dischi/api.php?genre=" + this.genre).then((res) => {
+        this.dischi = res.data;
+      });
+    },
+  },
   created() {
     axios.get("/php-ajax-dischi/api.php").then((res) => {
       this.dischi = res.data;
